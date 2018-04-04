38763ebd15c2c1eb70e2132cdad59df907a7a776
Julia <julia.volkova090@gmail.com>
2018-04-04 12:35:11 +0300

    Add project files

---
+++ js/app.js
@@ -0,0 +1,1 @@
+new Vue({    el: '#app',    data: {        title: 'Hello World!'    },    methods: {        changeTitle: function (event) {            this.title = event.target.value;        }    }});
