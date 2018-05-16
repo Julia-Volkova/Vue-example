4bb71ebe8b7d955d0c22721eae6794a12bdc80c5
Julia <julia.volkova090@gmail.com>
2018-05-16 14:46:03 +0300

    Exercise - Components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,8 +1,35 @@
 <template>
-  <app-servers></app-servers>
+  <div class="container">
+    <div class="row">
+      <header-server></header-server>
+    </div>
+    <hr>
+    <div class="row">
+      <list-info-servers></list-info-servers>
+      <servers-details></servers-details>
+    </div>
+    <hr>
+    <div class="row">
+      <footer-server></footer-server>
+    </div>
+  </div>
 </template>
 
 <script>
+  // Locally components, only for App.vue
+  import HeaderServer from './Header.vue';
+  import ListInfo from './ListInfo.vue';
+  import ServersDetails from './ServersDetails.vue';
+  import FooterServer from './Footer.vue';
+
+  export default {
+    components: {
+      'header-server': HeaderServer,
+      'list-info-servers': ListInfo,
+      'servers-details': ServersDetails,
+      'footer-server': FooterServer
+    }
+  }
 </script>
 
 <style>
---
+++ vue-cli/src/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-xs-12">
+    <footer>
+      <p>{{ result }}</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        result: 'All Servers are managed here'
+      }
+    }
+  }
+</script>
