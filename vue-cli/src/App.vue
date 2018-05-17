4b6b8a635bb77c1fcfe648909b001ad3d30306b1
Julia <julia.volkova090@gmail.com>
2018-05-17 15:16:41 +0300

    Communicating between components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,37 +1,26 @@
 <template>
   <div class="container">
     <div class="row">
-      <header-server></header-server>
-    </div>
-    <hr>
-    <div class="row">
-      <servers></servers>
-      <servers-details></servers-details>
-    </div>
-    <hr>
-    <div class="row">
-      <footer-server></footer-server>
+      <div class="col-xs-12">
+        <app-user></app-user>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  // Locally components, only for App.vue
-  import HeaderServer from './components/Shared/Header.vue';
-  import Servers from './components/Server/Servers.vue';
-  import ServersDetails from './components/Server/ServersDetails.vue';
-  import FooterServer from './components/Shared/Footer.vue';
+  import User from './components/User.vue';
 
   export default {
     components: {
-      'header-server': HeaderServer,
-      'servers': Servers,
-      'servers-details': ServersDetails,
-      'footer-server': FooterServer
+      'app-user': User
     }
   }
 </script>
 
 <style>
-
+  div.component {
+    border: 1px solid black;
+    padding: 30px;
+  }
 </style>
---
+++ vue-cli/src/components/UserEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="component">
+    <h3>You may edit the User here</h3>
+    <p>Edit me!</p>
+    <p>User age: {{ userAge }}</p>
+    <p>User name: {{ myName }}</p>
+    <button @click="resetFn()">Reset name</button>
+    <button @click="editAge">Edit age</button>
+  </div>
+</template>
+
+<script>
+  import { eventBus } from '../main';
+
+  export default {
+    props: {
+      userAge: Number,
+      myName: String,
+      resetFn: Function
+    },
+    methods: {
+      editAge() {
+        this.userAge = 30;
+//        this.$emit('ageWasEdited', this.userAge);
+//        eventBus.$emit('ageWasEdited', this.userAge);
+        eventBus.changeAge(this.userAge);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightgreen;
+  }
+</style>
