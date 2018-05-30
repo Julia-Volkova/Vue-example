75fd57d4a7db425c832ad313946e3a43d01ec56b
Julia <julia.volkova090@gmail.com>
2018-05-30 14:55:21 +0300

    Animation & transition on one element

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,46 +2,95 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Filters & Mixins</h1>
-        <p>{{ text | toUppercase | to-lowercase }}</p>
+        <h1>Animations</h1>
         <hr>
-        <button class="newItem" @click="fruits.push('Berries')">Add New Item</button>
-        <input type="text" v-model="filterText">
-        <ul>
-          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-        </ul>
-        <hr>
-        <app-list></app-list>
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br><br>
+        <button class="btn btn-primary" @click="show = !show">Show alert</button>
+        <br><br>
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-show="show">This is some Info</div>
+        </transition>
+        <transition name="slide" appear>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+        <transition
+          enter-class=""
+          enter-active-class="animated rubberBand"
+          leave-active-class="animated shake">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import List from './List.vue';
-  import { fruitMixin } from './fruitMixin';
-
   export default {
-    mixins: [fruitMixin],
     data() {
       return {
-        text: 'Hello here!'
+        show: true,
+        alertAnimation: 'fade'
       }
-    },
-    filters: {
-      toUppercase(value) {
-        return value.toUpperCase();
-      }
-    },
-    components: {
-      'app-list': List
     }
   }
 </script>
 
 <style>
-  .newItem {
-    display: block;
-    margin-bottom: 20px;
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 </style>
