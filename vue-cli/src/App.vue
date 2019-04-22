48a34f742c44773d794e199b0f9b459e344162ea
Юлия Волкова <jv@duotek.ru>
2019-04-22 10:32:31 +0300

    Routing

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,164 +1,18 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Animations</h1>
-        <hr>
-        <select v-model="alertAnimation" class="form-control">
-          <option value="fade">Fade</option>
-          <option value="slide">Slide</option>
-        </select>
-        <br><br>
-        <button class="btn btn-primary" @click="show = !show">Show alert</button>
-        <br><br>
-        <transition :name="alertAnimation">
-          <div class="alert alert-info" v-show="show">This is some Info</div>
-        </transition>
-
-        <transition name="slide" appear>
-          <div class="alert alert-info" v-if="show">This is some Info</div>
-        </transition>
-
-        <transition
-          enter-class=""
-          enter-active-class="animated rubberBand"
-          leave-active-class="animated shake">
-          <div class="alert alert-info" v-if="show">This is some Info</div>
-        </transition>
-
-        <transition :name="alertAnimation" mode="out-in">
-          <div class="alert alert-info" v-if="show" key="info">This is any Info</div>
-          <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
-        </transition>
-        <hr>
-        <button class="btn btn-primary" @click="load = !load">Load / remove element</button>
-        <br><br>
-        <transition
-          @before-enter="beforeEnter"
-          @enter="enter"
-          @after-enter="afterEnter"
-          @enter-cancelled="enterCancelled"
-
-          @before-leave="beforeLeave"
-          @leave="leave"
-          @after-leave="afterLeave"
-          @leave-cancelled="leaveCancelled">
-          <div style="width: 300px; height: 100px; background-color: lightgreen;" v-if="load"></div>
-        </transition>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        show: false,
-        load: true,
-        alertAnimation: 'fade',
-        elementWidth: 100
-      }
-    },
-    methods: {
-      beforeEnter(el) {
-        this.elementWidth = 100;
-        el.style.width = this.elementWidth + 'px';
-      },
-      enter(el, done) {
-        let round = 1;
-        const interval = setInterval(() => {
-          el.style.width = (this.elementWidth + round * 10) + 'px';
-          round++;
-          if (round > 20) {
-            clearInterval(interval);
-            done();
-          }
-        }, 20);
-      },
-      afterEnter(el) {
-
-      },
-      enterCancelled(el) {
-
-      },
-      beforeLeave(el) {
-        this.elementWidth = 300;
-        el.style.width = this.elementWidth + 'px';
-      },
-      leave(el, done) {
-        let round = 1;
-        const interval = setInterval(() => {
-          el.style.width = (this.elementWidth - round * 10) + 'px';
-          round++;
-          if (round > 20) {
-            clearInterval(interval);
-            done();
-          }
-        }, 20);
-      },
-      afterLeave(el) {
-
-      },
-      leaveCancelled(el) {
-
-      }
+    export default {
     }
-  }
 </script>
 
 <style>
-  .fade-enter {
-    opacity: 0;
-  }
-
-  .fade-enter-active {
-    transition: opacity 1s;
-  }
-
-  .fade-leave {
-
-  }
-
-  .fade-leave-active {
-    transition: opacity 1s;
-    opacity: 0;
-  }
-
-  .slide-enter {
-    opacity: 0;
-  }
-
-  .slide-enter-active {
-    animation: slide-in 1s ease-out forwards;
-    transition: opacity .5s;
-  }
-
-  .slide-leave {
-
-  }
-
-  .slide-leave-active {
-    animation: slide-out 1s ease-out forwards;
-    transition: opacity 1s;
-    opacity: 0;
-  }
-
-  @keyframes slide-in {
-    from {
-      transform: translateY(20px);
-    }
-    to {
-      transform: translateY(0);
-    }
-  }
-
-  @keyframes slide-out {
-    from {
-      transform: translateY(0);
-    }
-    to {
-      transform: translateY(20px);
-    }
-  }
 </style>
