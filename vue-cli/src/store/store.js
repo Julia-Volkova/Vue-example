7313454ae048e4a25e0c0709dc1108911f9f3873
Юлия Волкова <jv@duotek.ru>
2019-07-26 11:05:09 +0300

    different store used

--- vue-cli/src/store/store.js
+++ vue-cli/src/store/store.js
@@ -1,43 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter';
+import * as actions from './actions';
 
 Vue.use(Vuex);
 export const store = new Vuex.Store({
-  state: {
-    counter: 0,
-  },
-  getters: {
-    doubleCounter: state => {
-      return state.counter * 2;
-    },
-    stringCounter: state => {
-      return `${state.counter} Clicks`;
-    },
-  },
-  mutations: {
-    increment: (state, payload) => {
-      state.counter += payload;
-    },
-    decrement: (state, payload) => {
-      state.counter -= payload;
-    },
-  },
-  actions: {
-    increment: ({ commit }, payload) => {
-      commit('increment', payload);
-    },
-    decrement: ({ commit }, payload) => {
-      commit('decrement', payload);
-    },
-    asyncIncrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('increment', payload.by);
-      }, payload.duration);
-    },
-    asyncDecrement: ({ commit }, payload) => {
-      setTimeout(() => {
-        commit('decrement', payload.by);
-      }, payload.duration);
-    },
-  }
+	state: {
+		value: 0,
+	},
+	getters: {
+		value: state => {
+			return state.value;
+		}
+	},
+	mutations: {
+		updateValue: (state, payload) => {
+			state.value = payload;
+		},
+	},
+	actions,
+	modules: {
+		counter,
+	},
 });
